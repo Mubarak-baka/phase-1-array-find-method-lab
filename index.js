9e038df9ec6057a8b1bbb3a299f1f2f338fd7934
Mubarak-baka <Nassibmubarack@gmail.com>
2024-10-02 00:25:37 +0300

    array-find

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record){
+    const marks=[
+    {year:'2015',result:"W"},
+    {year:'2016',result:'N/A'},
+    {year:'2013',result:'L'}
+    ]     
+ const win = record.find(mark=>mark.result === "W");
+        return win ? win.year : undefined;
+}
+
+
+
